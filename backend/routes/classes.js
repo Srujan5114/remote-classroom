95ccc3af4e5a9641e3c19459591106bf7c9c827f
G Srujan <119472750+Srujan5114@users.noreply.github.com>
2026-04-18 02:34:34 +0530

    Add updateClassSession route to classes.js

--- backend/routes/classes.js
+++ backend/routes/classes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
   createClassSession,
-  getAllClassSessions
+  getAllClassSessions,
+  updateClassSession
 } = require('../controllers/classController');
 
 router.post('/', createClassSession);
 router.get('/', getAllClassSessions);
+router.put('/:id', updateClassSession);
 
 module.exports = router;
